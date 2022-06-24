b9e5e8201e469d332e4fa194a46d4cbebf6bc613
Ngwube Miracle Onyedika <92275870+Mongwube@users.noreply.github.com>
2022-06-24 12:43:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 // (me.name);
 // alert(me.height);
-var me = [ "Name: Ngwube Miracle", "Height: Average height","Country: Nigeria."]
-alert ( me)
+var me = "Name: Ngwube Miracle" 
+var height = "<br> Height: Average height"
+var country = "<br> Country: Nigeria."
+document.write(me, height, country)
